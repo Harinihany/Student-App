9c55cf2c9c61a95c87363e12deb075a00abff053
Harinihany <harinihany03@gmail.com>
2020-11-29 09:09:05 +0530

    added: welcome message

---
+++ Student.java
@@ -0,0 +1,8 @@
+package com.java.util.scanner;
+import java.util.*;
+public class Student {
+	public static void main(String[] args) {
+  
+  	System.out.println("Welcome to Student registration portal");
+	}
+}
